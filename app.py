3cca8dd340b369a231595ae2a1d80d717b2339e2
artarona <artaronacorreo@gmail.com>
2026-08-27 23:27:08 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -34,8 +34,7 @@ ADMIN_TOKEN = os.environ.get('ADMIN_TOKEN', '2205')
 # Primero intentar obtener la URL desde las variables de entorno
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if not DATABASE_URL:
-    DATABASE_URL = "postgresql://dantepropiedades_db_ucly_user:lXJBs0o3hGelsXG3y9EKA2giwZyBdcUZ@dpg-da8e1gon74is73dm4d90-a:5432/dantepropiedades_db_ucly?sslmode=require"
-
+    DATABASE_URL = "postgresql://dantepropiedades_db_ucly_user:lXJBs0o3hGelsXG3y9EKA2giwZyBdcUZ@dpg-da8e1gon74is73dm4d90-a.oregon-postgres.render.com:5432/dantepropiedades_db_ucly?sslmode=require"
 
 
 
